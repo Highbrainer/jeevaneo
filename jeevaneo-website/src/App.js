5cd25f8a848640646ebfc4a3ebc048526dc89654
Highbrainer <alexandre.borgoltz@gmai.com>
2023-01-24 16:56:36 +0100

    initial empty stub

---
+++ jeevaneo-website/src/App.js
@@ -0,0 +1,25 @@
+import logo from './big-blog-conseil.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Jeevaneo !
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
